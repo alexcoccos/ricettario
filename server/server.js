ba792668591d1a9c6a3cfc421f76f21a137f73f8
Alex Contursi <contursi.axel@gmail.com>
2017-03-13 09:30:20 +0100

    fintio

--- server/server.js
+++ server/server.js
@@ -13,8 +13,8 @@ var bodyParser = require('body-parser');
 
 //file configurazione del db
 require('./config/database.js');//servere per fare la connsessione
-require('./routes/routes.js')(app,express,bodyParser);
 require('./config/logger.js')(app);
+require('./routes/routes.js')(app,express,bodyParser);
 ////////////////////////////////////////////
 
 
